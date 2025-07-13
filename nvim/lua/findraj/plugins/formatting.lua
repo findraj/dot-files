fd2199c9f4601c11f945180e5b3a1ca97fcb674a
findraj <jankofindra@gmail.com>
2025-07-13 15:22:34 +0200

    nvim

---
+++ nvim/lua/findraj/plugins/formatting.lua
@@ -0,0 +1,34 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        c = { "prettier" },
+        cpp = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+        python = { "isort", "black" },
+        php = { { "pint", "php_cs_fixer" } },
+        blade = { "blade-formatter"},
+      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   async = false,
+      --   timeout_ms = 1000,
+      -- },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>fp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
